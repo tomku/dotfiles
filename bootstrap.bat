833f6c12cb189d85ed8623a3c3579315a1f88fe0
Tom Kurth <tom@losthalo.org>
2014-01-19 11:11:53 -0600

    Remove unnecessary bootstrap vimrc commands.

--- bootstrap.bat
+++ bootstrap.bat
@@ -6,9 +6,6 @@ cd ..
 
 if exist %USERPROFILE%\_vimrc goto GVIMRC
 echo set rtp+=%CD%\dotfiles\vim> %USERPROFILE%\_vimrc
-echo set rtp+=%CD%\.vim>> %USERPROFILE%\_vimrc
-
-REM echo let ^&runtimepath = printf('%%s,%%s', '%CD%\.vim\vimfiles', ^&runtimepath) >> %USERPROFILE%\_vimrc
 echo source %CD%\dotfiles\vim\vimrc >> %USERPROFILE%\_vimrc
 
 :GVIMRC
