5a066cdcdfa3bec0d76be3ff7d5377a21fc8ae37
Tom Kurth <tom@losthalo.org>
2014-01-19 10:58:27 -0600

    Fix wrong vimrc filenames.

--- bootstrap.bat
+++ bootstrap.bat
@@ -6,11 +6,11 @@ cd ..
 
 if exist %USERPROFILE%\_vimrc goto GVIMRC
 echo let ^&runtimepath = printf('%%s,%%s', '%CD%\vim\vimfiles', ^&runtimepath) > %USERPROFILE%\_vimrc
-echo source %CD%\dotfiles\vim\_vimrc >> %USERPROFILE%\_vimrc
+echo source %CD%\dotfiles\vim\vimrc >> %USERPROFILE%\_vimrc
 
 :GVIMRC
 if exist %USERPROFILE%\_gvimrc goto END
-echo source %CD%\dotfiles\vim\_gvimrc > %USERPROFILE%\_gvimrc
+echo source %CD%\dotfiles\vim\gvimrc > %USERPROFILE%\_gvimrc
 
 :END
 popd
