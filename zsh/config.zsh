87fddcfaa8f02a2ad891bfaeb52f178f96acd508
Tom Kurth <tom@losthalo.org>
2015-08-21 10:44:32 -0500

    Add support for foregrounding vim easily, fix titles for tmux.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,3 +39,45 @@ if [[ ${TERM} == "xterm" ]] ; then
     export TERM=xterm-256color
 fi
 
+# From http://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity/
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
+
+# Heavily modified from http://superuser.com/questions/497715/keep-tmux-title-from-fg-to-original-jobname
+fix_fg_title() {
+    local fg_title
+    local remove_parens="\(*\)"
+    if [[ $* == fg* ]]; then
+        # Numeric process ID like 'fg %1'
+        if [[ $* == *%<->* ]]; then
+            fg_title=($(jobs ${${(z)1}[2]}))
+        # Plain 'fg'
+        elif [[ $1 == "fg" ]]; then
+            fg_title=($(jobs %%))
+        # Named process like 'fg %vim' or 'fg vim'
+        else
+            local arg="${${(z)1}[2]}"
+            arg=${arg/\%}
+            fg_title=($(jobs | grep $arg))
+        fi
+        fg_title="$fg_title"
+        local title=${fg_title/${~remove_parens}}
+        title=${${(z)title}[4]}
+        title "$title"
+    else
+        fg_title="$1 end"
+        title ${${(z)fg_title}[1]}
+    fi
+}
+
+preexec_functions+=(fix_fg_title)
