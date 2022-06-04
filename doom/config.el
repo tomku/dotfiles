655b9011b2e17bef241448bfa913a22eb1433a26
Tom Kurth <tom@losthalo.org>
2022-06-04 17:44:14 -0500

    Enable all the Emacs power tools.

--- doom/config.el
+++ doom/config.el
@@ -52,6 +52,8 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(setq disabled-command-function nil)
+
 (setq scroll-margin 10)
 
 (setq projectile-project-search-path '("~/proj/"))
