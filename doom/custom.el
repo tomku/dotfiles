865a625d77aa1dc06702e9df01ae8db30d42e317
Tom Kurth <tom@losthalo.org>
2024-04-22 11:39:34 -0500

    Minor Doom updates.

--- doom/custom.el
+++ doom/custom.el
@@ -6,9 +6,11 @@
  '(ansi-color-names-vector
    ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
  '(custom-safe-themes
-   '("7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" default))
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "1d44ec8ec6ec6e6be32f2f73edf398620bb721afeed50f75df6b12ccff0fbb15" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "b5803dfb0e4b6b71f309606587dd88651efe0972a5be16ece6a958b197caeed8" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "c5ded9320a346146bbc2ead692f0c63be512747963257f18cc8518c5254b7bf5" "846b3dc12d774794861d81d7d2dcdb9645f82423565bfb4dad01204fa322dbd5" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" default))
  '(exwm-floating-border-color "#191b20")
  '(fci-rule-color "#5B6268")
+ '(flycheck-gfortran-include-path '("../src" "../app"))
+ '(flycheck-gfortran-language-standard "f2008ts")
  '(highlight-tail-colors
    ((("#333a38" "#99bb66" "green")
      . 0)
--- doom/init.el
+++ doom/init.el
@@ -104,7 +104,7 @@
        ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
-       ;;terraform         ; infrastructure as code
+       terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
@@ -149,8 +149,8 @@
        ;;ledger            ; be audit you can be
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nim               ; python + lisp at the speed of c
+       nix               ; I hereby declare "nix geht mehr!"
        ocaml             ; an objective camel
        (org +journal +roam2)      ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
