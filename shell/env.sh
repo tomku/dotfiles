9dccde960dce0b45322efd8a3fcd995e0938d9ee
Tom Kurth <tom@losthalo.org>
2024-07-27 10:22:12 -0500

    Switch from asdf to mise.
    mise[1] is a new-ish alternative to asdf that uses the same plugins, but
    has a more intuitive CLI and supports some extra goodies.
    
    [1] https://mise.jdx.dev

--- shell/env.sh
+++ shell/env.sh
@@ -22,9 +22,9 @@ if [ -d "/opt/homebrew/bin" ] ; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# asdf
-if [ -d "$HOME/.asdf" ] ; then
-    source $HOME/.asdf/asdf.sh
+# mise
+if command -v mise &>/dev/null; then
+    eval "$(mise activate zsh)"
 fi
 
 # Manually installed Zig
