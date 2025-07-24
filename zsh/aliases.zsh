b2bc72da26afa5db11037b2e4d05e282761cb131
Tom Kurth <tom@losthalo.org>
2025-07-24 02:23:12 -0500

    Add alias to update Calibre.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -88,6 +88,8 @@ fi
 alias yt-dlp-pl2='yt-dlp -o "%(playlist_index)02d - %(title)s.%(ext)s"'
 alias yt-dlp-pl3='yt-dlp -o "%(playlist_index)03d - %(title)s.%(ext)s"'
 
+alias update-calibre="sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin"
+
 rename_branch() {
     git branch -m $1 $2
     git branch --unset-upstream
