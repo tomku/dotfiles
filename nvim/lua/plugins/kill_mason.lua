04f0bcb1b3c279fe48ea343a3e92ba5ba0e3cead
Tom Kurth <tom@losthalo.org>
2025-08-18 14:19:46 -0500

    Cut down neovim config, disable Mason and DAP specifically.
    Just not worth the hassle of "automatic" installation and configuration
    when it's broken or outdated half the time.
    
    Complete rework of neovim config restarting with kickstart.nvim is
    coming when I have time for it.

---
+++ nvim/lua/plugins/kill_mason.lua
@@ -0,0 +1,9 @@
+return {
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "williamboman/mason.nvim",           enabled = false },
+  -- { "neovim/nvim-lspconfig",             enabled = false },
+  { "mfussenegger/nvim-dap",             enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim",      enabled = false },
+  { "rcarriga/nvim-dap-ui",              enabled = false },
+  { "nvim-neotest/neotest",              enabled = false },
+}
