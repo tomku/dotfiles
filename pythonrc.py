05460345d8d1e44e44e945757d171500c5b5c035
Tom Kurth <tom@losthalo.org>
2025-11-05 10:00:09 -0600

    Make pythonrc resilient to missing modules, disable unused import warnings.

--- pythonrc.py
+++ pythonrc.py
@@ -1,8 +1,27 @@
-from rich import pretty, print, inspect
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from pprint import pprint as pp
+# ruff: noqa: F401
+try:
+    from pprint import pprint as pp
+except ImportError:
+    pass
 
+try:
+    from rich import pretty, print, inspect
 
-pretty.install()
+    pretty.install()
+except ImportError:
+    pass
+
+try:
+    import pandas as pd
+except ImportError:
+    pass
+
+try:
+    import numpy as np
+except ImportError:
+    pass
+
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    pass
